d83b82fc928fa3c8c7d73cfbde8cf5e4928e7b59
ZhengkaiXu <695378091@qq.com>
2024-08-18 22:24:59 +0800

    1020 1021

---
+++ BasicLevel /1021.cpp
@@ -0,0 +1,19 @@
+//
+// Created by ZhengkaiXu on 2024/8/18.
+//
+#include <iostream>
+using namespace std;
+int main() {
+    string s;
+    cin >> s;
+    int a[10] = {0};
+    for (int i = 0; i < s.length(); i++) {
+        a[s[i] - '0']++;
+    }
+    for (int i = 0; i < 10; i++) {
+        if (a[i] != 0) {
+            printf("%d:%d\n", i, a[i]);
+        }
+    }
+    return 0;
+}
